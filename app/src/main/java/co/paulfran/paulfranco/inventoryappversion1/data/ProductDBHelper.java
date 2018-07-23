58e532245c57db3ba2ce244dc8c7e9aef3b3de1e
Paul Franco <paulfranco@me.com>
2018-07-23 01:47:56 -0700

    Created ProductDBHelper

--- app/src/main/java/co/paulfran/paulfranco/inventoryappversion1/data/ProductDBHelper.java
+++ app/src/main/java/co/paulfran/paulfranco/inventoryappversion1/data/ProductDBHelper.java
@@ -1,4 +1,42 @@
 package co.paulfran.paulfranco.inventoryappversion1.data;
 
-public class ProductDBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ProductDBHelper extends SQLiteOpenHelper {
+
+    // Name of the Database file
+    private static final String DATABASE_NAME = "shelter.db";
+
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Constructor
+    public ProductDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_PRODUCTS_TABLE = "CREATE TABLE " + ProductContract.ProductEntry.TABLE_NAME + "("
+                + ProductContract.ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ProductContract.ProductEntry.COLUMN_PRODUCT_NAME + "TEXT NOT NULL, "
+                + ProductContract.ProductEntry.COLUMN_PRODUCT_PRICE + "DECIMAL NOT NULL, "
+                + ProductContract.ProductEntry.COLUMN_PRODUCT_QTY + " INTEGER NOT NULL, "
+                + ProductContract.ProductEntry.COLUMN_PRODUCT_SUPPLIER_NAME + " TEXT NOT NULL, "
+                + ProductContract.ProductEntry.COLUMN_PRODUCT_SUPPLIER_PHONE + " TEXT NOT NULL );";
+
+        db.execSQL(SQL_CREATE_PRODUCTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Override Version
+    }
+
+
+
+
+
 }
